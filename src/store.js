8a5ab35e0300b8df7604eb911456f69b12e63798
arcodez <abelileior123@gmail.com>
2021-05-21 13:57:12 -0400

    update

--- src/store.js
+++ src/store.js
@@ -80,31 +80,26 @@ const reducerEntrenador = (state = initialState, action) => {
         titulares: state.titulares.concat(action.jugador),
         jugadores: state.jugadores.filter((j) => j.id !== action.jugador.id),
       };
-      break;
     case "AGREGAR_SUPLENTE":
       return {
         ...state,
         suplentes: state.suplentes.concat(action.jugador),
         jugadores: state.jugadores.filter((j) => j.id !== action.jugador.id),
       };
-      break;
     case "QUITAR_TITULAR":
       return {
         ...state,
         titulares: state.titulares.filter((j) => j.id !== action.jugador.id),
         jugadores: state.jugadores.concat(action.jugador),
       };
-      break;
     case "QUITAR_SUPLENTE":
       return {
         ...state,
         suplentes: state.suplentes.filter((j) => j.id !== action.jugador.id),
         jugadores: state.jugadores.concat(action.jugador),
       };
-      break;
     default:
       return state;
-      break;
   }
 };
 
